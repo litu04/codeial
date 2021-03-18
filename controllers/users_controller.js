60f2da7d45412e783c424363c01645d7dfb9bafe
litu04 <debadattakar96@gmail.com>
2021-03-18 12:42:08 +0530

    show details of signed-in user

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,9 +4,29 @@ const User = require('../models/user');
 module.exports.profile = function(req,res){
     //return res.send("<h1>Welcome to the Profile page</h1>");
 
-    return res.render('profile',{
-        title: 'profile'
-    });
+    //console.log('user_id',req.cookies.user_id);
+    
+    // to check in my cookies if user_id is present or not
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id,function(err,user){
+            if(err){
+                console.log("Error in finding the user",err);
+                return;
+            }
+            //console.log("User email:",user.email);
+            //console.log("user name", user.name);
+            if(user){
+                return res.render('profile',{
+                    title: 'profile',
+                    u: user
+                });
+            }
+            return res.redirect('/users/sign-in');
+        });
+    }else{
+        return res.redirect('/users/sign-in');
+    }
+    
 }
 
 // rendering the sign-up page
@@ -29,8 +49,8 @@ module.exports.signIn = function(req,res){
 // getting the sign-up data
 module.exports.create = function(req,res){
 
-    console.log("password:",req.body.password);
-    console.log("confirm-password:",req.body.confirm_password);
+    //console.log("password:",req.body.password);
+    //console.log("confirm-password:",req.body.confirm_password);
 
     // to check if password and confirm-password are same or not
     if(req.body.password != req.body.confirm_password){
@@ -38,14 +58,14 @@ module.exports.create = function(req,res){
     }
 
     // to check if the user already exist
-    console.log('email:',req.body.email);
+    //console.log('email:',req.body.email);
     //console.log("old-email:",email);
     User.findOne({email: req.body.email},function(err,user){
         if(err){
             console.log("Error in finding user presence while signing-up",err);
             return;
         }
-        console.log("********",user);
+        //console.log("********",user);
         
         // if user is not present than we will create the user
         if(!user){
@@ -54,7 +74,7 @@ module.exports.create = function(req,res){
                     console.log("Error in creating user while signing-up",err);
                     return;
                 }
-                console.log("New user: ",user);
+                //console.log("New user: ",user);
                 return res.redirect('/users/sign-in');
             });
         } else{
@@ -74,11 +94,11 @@ module.exports.createSession = function(req,res){
             console.log("Error in finding user presence while signing-in",err);
             return;
         }
-        console.log("******",user)
+        //console.log("******",user)
         
         // handle user found
-        console.log("old password",user.password);
-        console.log("new password",req.body.password);
+        //console.log("old password",user.password);
+        //console.log("new password",req.body.password);
         if(user){
             if(user.password != req.body.password){
                 return res.redirect('back');
@@ -89,7 +109,7 @@ module.exports.createSession = function(req,res){
             return res.redirect('/users/profile');
         }else {
             // if user not found
-            return res.redirect('back');
+            return res.redirect('/users/sign-up');
         }
     });
 }
